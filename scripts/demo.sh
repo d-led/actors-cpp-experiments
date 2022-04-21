bd8ead482ee45d706e8c51b704c0c96068162f37
Dmitry Ledentsov <dmlled@yahoo.com>
2022-04-21 23:35:37 +0200

    demo script

---
+++ scripts/demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# https://stackoverflow.com/a/246128/847349
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_DIR=build
+
+$SCRIPT_DIR/get-deps.sh
+
+$SCRIPT_DIR/build.sh
+
+$SCRIPT_DIR/../$BUILD_DIR/actor-experiments-cpp
---
+++ scripts/get-deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+vcpkg install sobjectizer
